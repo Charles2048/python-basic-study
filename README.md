6c501bb42a106caee065abfabaeb2fa139356b09
Charles2048 <chengzhaoxun1@126.com>
2017-11-29 23:26:43 +0800

    change

---
+++ README.md
@@ -0,0 +1,2 @@
+# python-basic-study
+# python-basic-study
---
+++ list.py
@@ -0,0 +1,15 @@
+lists=['test1','test2','test3']
+print(lists)
+print(lists[0])
+print(lists[-1])
+message=""+lists[-1]+" test4"
+print(message)
+i=len(lists)
+print(i)   
+lists=['test1','test2','test3']
+for list in lists:
+  print(list)
+  print('\n')
+list1=lists[0:3]
+for list2 in list1:
+  print(list2)
